b442a3a8eedc13865857a164d69fa66e7f653375
chris-bacon <chrisbacon2009@hotmail.com>
2018-06-17 23:26:20 +0100

    Added q to escape

--- app/Main.hs
+++ app/Main.hs
@@ -74,6 +74,7 @@ type B = ()
 
 handleEvent :: Calendar -> BrickEvent a b -> EventM c (Next Calendar)
 handleEvent c (VtyEvent (EvKey KEsc [])) = halt c
+handleEvent c (VtyEvent (EvKey (KChar 'q') [])) = halt c
 handleEvent c _ = continue c
 
 attributeMap :: AttrMap
