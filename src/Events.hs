cf92a611cff22070be94555abbff6461da1c37b0
chris-bacon <chrisbacon2009@hotmail.com>
2018-08-09 15:19:55 +0100

    Stuck on getting the right editor

--- src/Events.hs
+++ src/Events.hs
@@ -17,7 +17,12 @@ handleEvent c@(Calendar DayView _ _ _ _ _) (VtyEvent (EvKey (KChar 's') [])) = c
 handleEvent c@(Calendar DayView _ _ _ _ _) (VtyEvent (EvKey (KChar 'q') [])) = continue $ c & currentView .~ MonthView
 handleEvent c@(Calendar DayView _ _ _ _ _) (VtyEvent (EvKey KEnter [])) = continue $ c & currentView .~ EditView
 handleEvent c@(Calendar EditView _ _ _ _ _) (VtyEvent (EvKey KEnter [])) = continue $ c & currentView .~ DayView
--- handleEvent c@(Calendar EditView _ _ _ _ _) (VtyEvent event) = continue =<< handleEventLensed c (day . events . editor) E.handleEditorEvent event
+handleEvent c@(Calendar EditView _ _ _ _ day) (VtyEvent event) = continue =<< 
+    handleEventLensed 
+    c 
+    ((a day) . editor)
+    E.handleEditorEvent 
+    event
 handleEvent c (VtyEvent (EvKey (KChar 'r') [])) = continue $ c & focusedDay .~ c ^. currentDay
 handleEvent c (VtyEvent (EvKey (KChar 'w') [])) = continue $ c & focusedDay -~ 7
 handleEvent c (VtyEvent (EvKey (KChar 'a') [])) = continue $ c & focusedDay -~ 1
@@ -26,4 +31,11 @@ handleEvent c (VtyEvent (EvKey (KChar 'd') [])) = continue $ c & focusedDay +~ 1
 handleEvent c (VtyEvent (EvKey KEnter [])) = continue $ c & currentView .~ DayView
 handleEvent c (VtyEvent (EvKey (KChar 'q') [])) = halt c
 handleEvent c _ = continue c
+
+
+a :: Day -> HourInfo
+a day = getCurrentHourInfo (day ^. currentHour) (day ^. hourInfo)
+
+getCurrentHourInfo :: Int -> [HourInfo] -> HourInfo
+getCurrentHourInfo n (h:hs) = if n == (h ^. time) then h else getCurrentHourInfo n hs
 
